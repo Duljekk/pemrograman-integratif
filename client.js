d1b4043d47dd1bccec28e8e81ba365d8fe7495e5
Duljekk <abdulzakisr@gmail.com>
2023-04-02 23:16:55 +0700

    Add READ feature.

--- client.js
+++ client.js
@@ -4,22 +4,38 @@ const protoLoader = require('@grpc/proto-loader')
 let PROTO_PATH = 'mahasiswa.proto'
 
 const packageDef = protoLoader.loadSync(PROTO_PATH, {});
-const grpcObject = grpc.loadPackageDefinition(packageDef)
-
-const MahasiswaPackage = grpcObject.MahasiswaPackage
+const MahasiswaPackage = grpc.loadPackageDefinition(packageDef).MahasiswaPackage;
 
 const client = new MahasiswaPackage.MahasiswaService('localhost:3000', grpc.credentials.createInsecure());
 
-const id_mahasiswa = '';
-const nama = 'Adimasdefatra Bimasena';
-const nrp = '5027211040';
-const nilai = '99';
+const id_mahasiswa = 1;
+const nama = 'Rendy Anfi Yudha';
+const nrp = '5027211006';
+const nilai = '94';
 
-client.addMahasiswa({ id_mahasiswa, nama, nrp, nilai }, (err, response) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
+// client.AddMahasiswa({ id_mahasiswa, nama, nrp, nilai }, (err, response) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+// });
 
-  console.log(response.message);
-});
+client.GetMahasiswa({ id_mahasiswa }, (err, response) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(response);
+  });
+
+  
+// const mahasiswaId = { id_mahasiswa: 1 };
+// client.DeleteMahasiswa(mahasiswaId, (error, response) => {
+//   if (error) {
+//     console.error(error);
+//     return;
+//   }
+//   console.log(response.message);
+// });
+
+  // console.log(response.message);
--- server.js
+++ server.js
@@ -5,31 +5,12 @@ const mysql = require('mysql2');
 let PROTO_PATH = 'mahasiswa.proto';
 
 const packageDef = protoLoader.loadSync(PROTO_PATH, {});
-const grpcObject = grpc.loadPackageDefinition(packageDef);
-
-const mahasiswaPackage = grpcObject.MahasiswaPackage;
+const MahasiswaPackage = grpc.loadPackageDefinition(packageDef).MahasiswaPackage;
 
 const server = new grpc.Server();
 
-server.addService(mahasiswaPackage.MahasiswaService.service, {
-  addMahasiswa: addMahasiswa,
-});
-
-server.bindAsync('127.0.0.1:3000', grpc.ServerCredentials.createInsecure(), (error, port) => {
-  if (error) throw error;
-
-  console.log(`Server running at ${port}`);
-  server.start();
-});
-
-const connection = mysql.createConnection({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '',
-  database: 'pemrograman_integratif',
-});
-
-function addMahasiswa(call, callback) {
+server.addService(MahasiswaPackage.MahasiswaService.service, {
+  AddMahasiswa(call, callback) {
     
     const id_mahasiswa = call.request.id_mahasiswa;
     const nama = call.request.nama;
@@ -49,4 +30,115 @@ function addMahasiswa(call, callback) {
         callback(null, { message: 'Mahasiswa berhasil ditambahkan' });
       }
     );
+  },
+  GetMahasiswa(call, callback) {
+    const id_mahasiswa = call.request.id_mahasiswa;
+    connection.query(
+      `SELECT * FROM mahasiswa WHERE id_mahasiswa = 1`,
+      [id_mahasiswa],
+      (error, results) => {
+        if (error) {
+          console.error('Error:', error);
+          callback(error, null);
+          return;
+        }
+        // if (results.length === 0) {
+        //   callback(`Mahasiswa dengan id ${id_mahasiswa} tidak ditemukan`, null);
+        //   return;
+        // }
+        const mahasiswa = results[0];
+        callback(null, {
+          id_mahasiswa: mahasiswa.id_mahasiswa,
+          nama: mahasiswa.nama,
+          nrp: mahasiswa.nrp,
+          nilai: mahasiswa.nilai
+        });
+      }
+    );
+  },
+  DeleteMahasiswa: deleteMahasiswa,
+});
+
+server.bindAsync('127.0.0.1:3000', grpc.ServerCredentials.createInsecure(), (error, port) => {
+  if (error) throw error;
+
+  console.log(`Server running at ${port}`);
+  server.start();
+});
+
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '',
+  database: 'pemrograman_integratif',
+});
+
+// function AddMahasiswa(call, callback) {
+    
+//     const id_mahasiswa = call.request.id_mahasiswa;
+//     const nama = call.request.nama;
+//     const nrp = call.request.nrp;
+//     const nilai = call.request.nilai;
+  
+//     connection.query(
+//       `INSERT INTO mahasiswa (id_mahasiswa, nama, nrp, nilai) VALUES (?, ?, ?, ?)`,
+//       [id_mahasiswa, nama, nrp, nilai],
+//       (error) => {
+//         if (error) {
+//           console.error('Error:', error);
+//           callback(error, null);
+//           return;
+//         }
+  
+//         callback(null, { message: 'Mahasiswa berhasil ditambahkan' });
+//       }
+//     );
+//   }
+
+  // function GetMahasiswa(call, callback) {
+  //   const mahasiswaId = call.request.id_mahasiswa;
+  //   connection.query(
+  //     `SELECT * FROM mahasiswa WHERE id_mahasiswa = ?`,
+  //     [mahasiswaId],
+  //     (error, results) => {
+  //       if (error) {
+  //         console.error('Error:', error);
+  //         callback(error, null);
+  //         return;
+  //       }
+  //       if (results.length === 0) {
+  //         callback(`Mahasiswa dengan id ${mahasiswaId} tidak ditemukan`, null);
+  //         return;
+  //       }
+  //       const mahasiswaItem = results[0];
+  //       callback(null, {
+  //         id_mahasiswa: mahasiswaItem.id_mahasiswa,
+  //         nama: mahasiswaItem.nama,
+  //         nrp: mahasiswaItem.nrp,
+  //         nilai: mahasiswaItem.nilai
+  //       });
+  //     }
+  //   );
+  // }
+
+  function deleteMahasiswa(call, callback) {
+    const mahasiswaId = call.request.id;
+  
+    connection.query(
+      `DELETE FROM mahasiswa WHERE id_mahasiswa = ?`,
+      [mahasiswaId],
+      (error, results) => {
+        if (error) {
+          console.error('Error:', error);
+          callback(error, null);
+          return;
+        }
+        if (results.affectedRows === 0) {
+          callback(`Mahasiswa dengan id ${mahasiswaId} tidak ditemukan`, null);
+          return;
+        }
+        callback(null, {});
+      }
+    );
   }
+  
